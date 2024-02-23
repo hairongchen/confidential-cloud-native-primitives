4bb89ccff4300e9b38d229673372bad07c1fcf9f
hairongchen <hairong.chen@intel.com>
2024-02-23 16:38:40 +0800

    c31

--- sdk/rust/cctrusted_ccnp/src/client.rs
+++ sdk/rust/cctrusted_ccnp/src/client.rs
@@ -298,7 +298,7 @@ impl CcnpServiceClient {
             if line.contains(docker_pattern) {
                 if let e = line.split(docker_pattern).last() {
                     let (id, _) = e.unwrap().split_once("/").unwrap();
-                    Ok(id)
+                    return Ok(id);
                 } else {
                     return Err(anyhow!("[get_container_id] incorrect docker container info in /proc/self/mountinfo!"));
                 }
@@ -312,7 +312,7 @@ impl CcnpServiceClient {
                 if let e = line.split(k8s_pattern).last() {
                     let (left, _) = e.unwrap().split_once("/").unwrap();
                     let id = left.replace("-", "_");
-                    Ok(id)
+                    return Ok(id);
                 } else {
                    return Err(anyhow!("[get_container_id] incorrect k8s pod container info in /proc/self/mountinfo!"));
                 }
