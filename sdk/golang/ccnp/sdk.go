e5391f606b372a10bb552833be811d9eb23ee29b
hairongchen <hairong.chen@intel.com>
2024-03-06 14:13:39 +0800

    c20

--- sdk/golang/ccnp/sdk.go
+++ sdk/golang/ccnp/sdk.go
@@ -41,3 +41,35 @@ func (s *SDK) GetCCReport(nonce string, userData string, _ any) (pb.GetCcReportR
 func (s *SDK) DumpCCReport(reportBytes []byte) error {
 	return nil
 }
+
+// GetCCMeasurement implements cctrusted_base.CCTrustedAPI.
+func (s *SDK) GetCCMeasurement(index int, alg cctrusted_base.TCG_ALG) (cctrusted_base.TcgDigest, error) {
+	emptyRet := cctrusted_base.TcgDigest{}
+	return emptyRet, nil
+}
+
+// GetMeasurementCount implements cctrusted_base.CCTrustedAPI.
+func (s *SDK) GetMeasurementCount() (int, error) {
+	return 4, nil
+}
+
+// ReplayCCEventLog implements cctrusted_base.CCTrustedAPI.
+// func (s *SDK) ReplayCCEventLog(formatedEventLogs []cctrusted_base.FormatedTcgEvent) map[int]map[cctrusted_base.TCG_ALG][]byte {
+func (s *SDK) ReplayCCEventLog(formatedEventLogs []cctrusted_base.FormatedTcgEvent) error {
+	return nil
+}
+
+// GetDefaultAlgorithm implements cctrusted_base.CCTrustedAPI.
+// func (s *SDK) GetDefaultAlgorithm() cctrusted_base.TCG_ALG {
+func (s *SDK) GetDefaultAlgorithm() error {
+	return nil
+}
+
+// SelectEventlog implements CCTrustedAPI.
+// func (s *SDK) GetCCEventLog(start int32, count int32) (*cctrusted_base.EventLogger, error) {
+func (s *SDK) GetCCEventLog(start int32, count int32) error {
+
+	el, err := s.internelEventlog()
+
+	return nil
+}
